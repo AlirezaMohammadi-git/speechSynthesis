f7ac1ba3ab77f6734e872df1cc44fbad79d45c0a
Joe <a.m.sadra4@gmail.com>
2025-04-03 23:42:23 +0330

    adding hint for unsupported browsers

--- app.js
+++ app.js
@@ -1,54 +1,66 @@
-
-
-
 const msg = new SpeechSynthesisUtterance();
 
-      let voices = [];
-      const voicesDropdown = document.querySelector('[name="voice"]');
-      const options = document.querySelectorAll(
-        '[type="range"], [name="text"]'
-      );
-      const speakButton = document.querySelector("#speak");
-      const stopButton = document.querySelector("#stop");
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+const container = document.querySelector(".voiceinator");
 
-      // doc : https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
-      function populateVoiceList() {
-        voices = this.getVoices();
-        console.log(voices);
-        voicesDropdown.innerHTML = voices
-          .map(
-            (voice) =>
-              `<option value="${voice.name}">${voice.name} (${voice.lang}) ${
-                voice.default ? "- default" : ""
-              }</option>`
-          )
-          .join("");
-      }
+// doc : https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+function populateVoiceList() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(
+      (voice) =>
+        `<option value="${voice.name}">${voice.name} (${voice.lang}) ${
+          voice.default ? "- default" : ""
+        }</option>`
+    )
+    .join("");
+  if (voices.length !== 0) {
+    hideError();
+  }
+}
 
-      function setVoice() {
-        msg.voice = voices.find((voice) => voice.name === this.value);
-        toggle();
-      }
+function showError() {
+  const parent = container;
+  const child = stopButton;
+  const errorText = document.createElement("p");
+  errorText.setAttribute("id", `error-text`);
+  errorText.textContent = "This browser hasn't any built-in voice!";
+  errorText.style.color = "red";
+  parent.insertBefore(errorText, child);
+}
 
-      function toggle(startOver = true) {
-        window.speechSynthesis.cancel();
-        if (startOver) {
-          window.speechSynthesis.speak(msg);
-          console.log(msg);
-        }
-      }
+function hideError() {
+  const elem = document.querySelector("#error-text");
+  if (elem) {
+    container.removeChild(elem);
+  }
+}
 
-      function setOption() {
-        console.log(this.name, this.value);
-        msg[this.name] = this.value;
-      }
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name === this.value);
+  toggle();
+}
 
-      window.speechSynthesis.addEventListener(
-        "voiceschanged",
-        populateVoiceList
-      );
-      voicesDropdown.addEventListener("change", setVoice);
-      options.forEach((option) => option.addEventListener("change", setOption));
-      speakButton.addEventListener("click", toggle);
-      stopButton.addEventListener("click", toggle.bind(null, false));
+function toggle(startOver = true) {
+  window.speechSynthesis.cancel();
+  if (startOver) {
+    window.speechSynthesis.speak(msg);
+    console.log(msg);
+  }
+}
 
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+}
+
+window.speechSynthesis.addEventListener("voiceschanged", populateVoiceList);
+voicesDropdown.addEventListener("change", setVoice);
+options.forEach((option) => option.addEventListener("change", setOption));
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", toggle.bind(null, false));
+showError();
